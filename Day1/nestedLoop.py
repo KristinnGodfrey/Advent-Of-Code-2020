12fb389c096e51bd2453861ce0bfd8b95471125a
KristinnGodfrey <kristinngodfrey@gmail.com>
2020-12-14 23:55:53 +0000

    Day 1

--- Day1/nestedLoop.py
+++ Day1/nestedLoop.py
@@ -1,9 +1,11 @@
 import sys
 with open(sys.argv[1], 'r') as f:
     lines = [int(line.rstrip()) for line in f]
-    
+
+ans = 0
 for li in lines:
     for i in lines:
-        if(i+li==2020):
-            print(i*li)
-            continue
+        if(i != li and i+li==2020):
+            ans = i*li            
+print(ans)
+            
