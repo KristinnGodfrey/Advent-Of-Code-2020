a93bd3eb996d33b9b09f1eb37408e21871106514
KristinnGodfrey <kristinngodfrey@gmail.com>
2020-12-22 19:00:27 +0000

    Day 4

--- Day4/passports.py
+++ Day4/passports.py
@@ -1,18 +1,36 @@
 import re
-
-counter = 0
-
 with open('input.txt') as f:
-    Output = f.read()
-Passar = Output.split('\n\n')
-ValidPassports = 0
-for Passi in Passar:
-    SplitPassalina2 = re.split('(\s|\n)',Passi)
-    Fields = []
-    for Field in SplitPassalina2:
-        Fields.append(Field.split(':')[0])
-    if 'byr' in Fields and 'iyr' in Fields and 'eyr' in Fields and 'hgt' in Fields and 'ecl' in Fields and 'hcl' in Fields and 'pid' in Fields :
-        print(counter)
-        print(Field)
-        ValidPassports += 1
-    counter += 1
+    lines = f.read()
+
+# init
+states = {'byr': False,'iyr': False,'eyr': False,'hgt': False,'hcl': False,'ecl': False,'pid': False,'cid': False }
+valid = 0
+
+# set states 
+lines = lines.split('\n\n') 
+for li in lines:
+    li = re.split('\s|\\n',li) 
+
+    for i in li:       
+        if 'byr' in i: states['byr'] = True 
+        if 'iyr' in i: states['iyr'] = True
+        if 'eyr' in i: states['eyr'] = True
+        if 'hgt' in i: states['hgt'] = True
+        if 'hcl' in i: states['hcl'] = True
+        if 'ecl' in i: states['ecl'] = True
+        if 'pid' in i: states['pid'] = True
+
+    # validate    
+    counter = 0
+    state = states.values()
+    
+    for s in state:
+        if s == True:
+            counter += 1
+    if counter >= 7: 
+        valid +=1
+
+    # reset states
+    states = {'byr': False,'iyr': False,'eyr': False,'hgt': False,'hcl': False,'ecl': False,'pid': False,'cid': False }
+    
+print(valid)
