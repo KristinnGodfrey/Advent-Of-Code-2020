35b9423225409d6a1169d429ae44576cc1e3bef5
KristinnGodfrey <kristinngodfrey@gmail.com>
2020-12-22 20:20:56 +0000

    ver 2

--- Day4/passRestrict.py
+++ Day4/passRestrict.py
@@ -3,11 +3,28 @@ with open('input.txt') as f:
     lines = f.read()
 
 def checkByr(state):
-    print("noice")
-    state = int(state)
-    if state >= 1920 and state <= 2002:
-        state = str(state)
-        return re.match('[1920-2002]', state) #and state >= 1920 and state <= 2002
+    return int(state) >= 1920 and int(state) <= 2002
+
+def checkIyr(state):
+    return int(state) >= 2010 and int(state) <= 2020
+
+def checkEyr(state):
+    return int(state) >= 2020 and int(state) <= 2030
+
+#Virkar kannski ekki
+def checkHgt(state):
+    if 'cm' in state:
+        state = state.split('cm')
+        state[0] = int(state[0])      
+        if state[0] >= 150 and state[0] <= 193: return True
+    elif 'in' in state:
+        state = state.split('in')
+        state[0] = int(state[0])      
+        if state[0] >= 59 and state[0] <= 76: 
+            print(state)
+            return True
+    return True
+
 # init
 states = {'byr': False,'iyr': False,'eyr': False,'hgt': False,'hcl': False,'ecl': False,'pid': False,'cid': False }
 valid = 0
@@ -18,12 +35,17 @@ for li in lines:
     li = re.split('\s|\\n',li) 
 
     for i in li:
-        i = i.split(':')    
-        if 'byr' in i and checkByr(i[1]):
-            states['byr'] = True 
-        if 'iyr' in i: states['iyr'] = True
-        if 'eyr' in i: states['eyr'] = True
-        if 'hgt' in i: states['hgt'] = True
+        i = i.split(':')            
+        if 'byr' in i and checkByr(i[1]): states['byr'] = True 
+        if 'iyr' in i and checkIyr(i[1]): states['iyr'] = True
+        if 'eyr' in i and checkEyr(i[1]): states['eyr'] = True            
+        if 'hgt' in i and checkHgt(i[1]): states['hgt'] = True           
+            # if 'cm' in i[1]:
+            #     print(i[1])
+            # elif 'in' in i[1]:
+            #     print(i[1])
+            # states['hgt'] = True
+            # print(i)
         if 'hcl' in i: states['hcl'] = True
         if 'ecl' in i: states['ecl'] = True
         if 'pid' in i: states['pid'] = True
